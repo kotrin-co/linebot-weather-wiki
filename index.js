5c7e2d60b1a195b9da89957182106f79daf74890
user <user@rrcs-172-254-99-32.nyc.biz.rr.com>
2020-05-04 13:53:50 +0900

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const line = require('"@line/bot-sdk');
+const line = require('@line/bot-sdk');
 
 const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,

4b553a052955e6366f681f2ccea16d76c4ba78c8
user <user@rrcs-172-254-99-32.nyc.biz.rr.com>
2020-05-04 13:47:16 +0900

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const line = require('"@line/bot-sdk');
+
+const config = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET_KEY
+};
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,5 +17,10 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/g/',(req,res) => res.json({method:"こんにちはgetさん"}))
   .post('/p/',(req,res) => res.json({method:"こんにちはpostさん"}))
-  .post('/hook/',(req,res) => res.json({test:"hook"}))
+  .post('/hook/',line.middleware(config),(req,res) => lineBot(req,res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  const lineBot = (req,res) => {
+    res.json({test:'hook'});
+    console.log('pass');
+  }
